b15d39016fd46e8a03c59cfaab771f71f1d83ba8
suzuki618 <daxianglingmu05@gmail.com>
2023-11-14 15:07:19 +0900

    できた。

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+	static void kadaiA(){
+	for (int i = 1; i <= 10; i++) {
+		System.out.println("現在"+ i + "回目のループです。");
+		
+	}
+		
+	}
+	
+	static void kadaiB(){
+		
+	}
+	
+	static void kadaiC(){
+		
+	}
+}
