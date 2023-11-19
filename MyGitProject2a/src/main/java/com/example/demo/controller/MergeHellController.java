a3c20cba92ea947a23090df953401380b9c51f0c
mayu08672 <S0607@22S0607>
2023-11-20 08:04:43 +0900

    更新した

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		//kadaiA();
+		//kadaiB();
+		//kadaiC();
+	}
+	//void kadai〇(){
+	//ここに課題を書く。メソッド名変えてね。
+	//}
+}
